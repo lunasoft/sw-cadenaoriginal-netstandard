6e44b5040246723d11558826ac2a26e69484874e
martinf12 <martin.flores@sw.com.mx>
2024-07-09 08:24:14 -0600

    Recursos para sellar CP 3.1

--- test/sw-cadenaoriginal-test/UT_CFDI40.cs
+++ test/sw-cadenaoriginal-test/UT_CFDI40.cs
@@ -114,5 +114,25 @@ namespace sw_cadenaoriginal_test
             string cadena = CadenaOriginal.CadenaOriginalCFDI(xml, "4.0");
             Assert.True(cadena.Equals("||4.0|Serie|Folio1|2024-01-12T00:00:00|99|30001000000500003416|CondicionesDePago|400|AMD|1|400.06|I|02|PPD|20000|EKU9003173C9|ESCUELA KEMPER URGATE|601|XEXX010101000|U.S. & SW|20000|USA|123456789|616|CP01|50211503|131494-1055|2|H87|Pieza|Cigarros|200.00|400.00|02|1|002|Tasa|0.160000|0.16|1|001|Tasa|0.100000|0.10|001|0.10|0.10|1|002|Tasa|0.160000|0.16|0.16|2.0|A1|0|FOB|16.9912|25.56|CALLE DEL PAPEL|0214|01|014|QUE|MEX|76199|123456789|ST. A|TX|USA|00000|131494-1055|2402200100|117.64|01|12.78|25.56||"), cadena);
         }
+        /// <summary>
+        /// Generar cadena original de un CFDI 4.0 de tipo Ingreso con carta porte 3.1 especificando la versión
+        /// </summary>
+        [Fact]
+        public void UT_CFDI40_Ingreso_CartaPorte31()
+        {
+            string xml = Encoding.UTF8.GetString(File.ReadAllBytes("Resources/CFDI40/CFDI31_Ingreso.xml"));
+            string cadena = CadenaOriginal.CadenaOriginalCFDI(xml, "4.0");
+            Assert.True(cadena.Equals("||4.0|SerieCCP31|CP3.1|2024-06-24T00:00:55|01|30001000000500003416|100.00|MXN|100.00|I|01|PUE|42501|EKU9003173C9|ESCUELA KEMPER URGATE|601|URE180429TM6|UNIVERSIDAD ROBOTICA ESPAÑOLA|86991|601|S01|78101800|UT421511|1|H87|Pieza|Transporte de carga por carretera|100.00|100.00|01|3.1|CCCBCD94-870A-4332-A52A-A52AA52AA52A|No|1|Sí|01|01|Origen|OR101010|URE180429TM6|NombreRemitenteDestinatario1|2023-08-01T00:00:00|Calle1|211|212|1957|13|casa blanca|011|CMX|MEX|13250|Destino|DE202020|URE180429TM6|NombreRemitenteDestinatario2|2023-08-01T00:00:01|1|Calle2|214|215|0347|23|casa negra|004|COA|MEX|25350|1.0|XBX|1|Sí|11121900|Accesorios de equipo de telefonía|1.0|XBX|No|DenominacionGenericaProd1|DenominacionDistintivaProd1|Fabricante1|2003-04-02|LoteMedic1|01|01|RegistroSanita1|1|6309000100|1|OR101010|DE202020|TPAF01|NumPermisoSCT1|VL|1|plac892|2020|AseguraRespCivil|123456789|CTR004|VL45K98|01|URE180429TM6|NumLicencia1|NombreFigura1|Calle1|NumeroExterior1|NumeroInterior1|Colonia1|Localidad1|Referencia1|Municipio1|Estado1|AFG|CodigoPosta1||"), cadena);
+        }
+        /// <summary>
+        /// Generar cadena original de un CFDI 4.0 de tipo Traslado con carta porte 3.1 especificando la versión
+        /// </summary>
+        [Fact]
+        public void UT_CFDI40_Tralsado_CartaPorte31()
+        {
+            string xml = Encoding.UTF8.GetString(File.ReadAllBytes("Resources/CFDI40/CFDI31_Traslado.xml"));
+            string cadena = CadenaOriginal.CadenaOriginalCFDI(xml, "4.0");
+            Assert.True(cadena.Equals("||4.0|Serie|Folio|2024-06-18T00:00:55|30001000000500003416|0|XXX|0|T|01|42501|EKU9003173C9|ESCUELA KEMPER URGATE|601|EKU9003173C9|ESCUELA KEMPER URGATE|42501|601|S01|78101800|UT421511|1|H87|Pieza|Transporte de carga por carretera|100.00|100.00|01|3.1|CCCBCD94-870A-4332-A52A-A52AA52AA52A|No|1|Sí|01|01|Origen|OR101010|EKU9003173C9|NombreRemitenteDestinatario1|2023-08-01T00:00:00|Calle1|211|212|1957|13|casa blanca|011|CMX|MEX|13250|Destino|DE202020|EKU9003173C9|NombreRemitenteDestinatario2|2023-08-01T00:00:01|1|Calle2|214|215|0347|23|casa negra|004|COA|MEX|25350|1.0|XBX|1|Sí|11121900|Accesorios de equipo de telefonía|1.0|XBX|No|DenominacionGenericaProd1|DenominacionDistintivaProd1|Fabricante1|2028-01-01|LoteMedic1|RegistroSanita1|1|1|OR101010|DE202020|TPAF01|NumPermisoSCT1|VL|1|plac892|2020|AseguraRespCivil|123456789|CTR004|VL45K98|01|EKU9003173C9|a234567890|NombreFigura||"), cadena);
+        }
     }
 }
